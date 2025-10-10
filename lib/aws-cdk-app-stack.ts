d3401974c85ce4ec8caf450d54b0ec3701d12ac6
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2025-10-11 01:06:32 +0200

    feat: initial aws resource created

--- lib/aws-cdk-app-stack.ts
+++ lib/aws-cdk-app-stack.ts
@@ -1,6 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expirationInDays: number) {
+    super(scope, id);
+
+    // L3 Bucket
+    new Bucket(this, 'L3Bucket', {
+      lifecycleRules: [
+        {
+          expiration: cdk.Duration.days(expirationInDays)
+        }
+      ]
+    });
+  }
+}
+
+
 
 export class AwsCdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,9 +25,49 @@ export class AwsCdkAppStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // L1 Bucket 
+    new CfnBucket(this, 'MyL1Bucket', {
+      lifecycleConfiguration: {
+        rules: [
+          {
+            status: 'Enabled',
+            expirationInDays: 2,
+            id: 'MyL1BucketRule'
+          }
+        ]
+      }
+    });
+
+
+    // L2 Bucket
+    const myL2Bucket = new Bucket(this, 'MyL2Bucket', {
+      lifecycleRules: [
+        {
+          expiration: cdk.Duration.days(2)
+        }
+      ]
+    });
+
+    console.log('L2 Bucket Name:', myL2Bucket.bucketName);
+
+     const duration = new cdk.CfnParameter(this, 'duration', {
+      default: 7,
+      maxValue: 30,
+      minValue: 1,
+      type: 'Number',
+      description: 'A sample parameter'
+    });
+
+    // L3 Bucket
+    new L3Bucket(this, 'MyL3Bucket', duration.valueAsNumber);
+
+    new cdk.CfnOutput(this, 'L2BucketName', {
+      value: myL2Bucket.bucketName,
+      description: 'The name of the L2 S3 Bucket',
+      exportName: 'L2BucketName'
+    });
+
+   
+
   }
 }
