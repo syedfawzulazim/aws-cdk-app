02af50580997b4ab2ba31fe8623f61f3ad4a4e35
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2025-10-11 23:17:30 +0200

    feat: add tags

--- bin/aws-cdk-app.ts
+++ bin/aws-cdk-app.ts
@@ -2,9 +2,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { AwsCdkAppStack } from '../lib/aws-cdk-app-stack';
 import { SecondStack } from '../lib/second-stack';
+import { Tagger } from './Tagger';
 
 const app = new cdk.App();
 const awsAppStack =  new AwsCdkAppStack(app, 'AwsCdkAppStack');
 new SecondStack(app, 'SecondStack', {
   tagetBucketArn: awsAppStack.photoBucketArn
 });
+
+const tagger = new Tagger('Project', 'AwsCdkApp');
+cdk.Aspects.of(app).add(tagger);
