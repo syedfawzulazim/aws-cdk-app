2562eee8e13b7f1495a9cd375306b026d980b97a
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2025-10-26 20:27:25 +0100

    delete

--- lambda-project/src/infra/utils.ts
+++
@@ -1,6 +0,0 @@
-import { Fn, Stack } from "aws-cdk-lib";
-
-export function getSuffixFromStack(stack: Stack): string {
-    const shortStackId = Fn.select(2, Fn.split('/', stack.stackId));
-    return Fn.select(4, Fn.split('-', shortStackId));
-}
